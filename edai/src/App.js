9b456df8820d4b95bacdcc45d1aae0d80e85e4d8
Montanah <mititi.isaac@gmail.com>
2023-06-25 15:12:42 +0300

    MVP edit

--- edai/src/App.js
+++ edai/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import edailogo from './EdAI Logo.png';
 
@@ -66,20 +66,95 @@ function AboutUsButton() {
 
 function SchoolSection() {
   return (
-    <section id="school">
-      <h3 id="school-title">Level</h3>
-      <button id="school-list-button"><strong>High School</strong></button>
-      <button id="school-list-button"><strong>College/University</strong></button>
-    </section>
+    <React.Fragment>
+      <button id="school-list-button">
+        <strong>High School</strong>
+      </button>
+      <button id="school-list-button">
+        <strong>College/University</strong>
+      </button>
+    </React.Fragment>
   );
 }
 
-function TopicSection() {
+function TopicDisplay() {
+  const isSubjectSelected = true;
+
+  let content;
+  if (isSubjectSelected) {
+    const topics = [
+      {id: 1, title: "Topic 1"},
+      {id: 2, title: "Topic 2"},
+      {id: 3, title: "Topic 3"},
+      {id: 4, title: "Topic 4"},
+      {id: 5, title: "Topic 5"}
+    ];
+    content = (
+      <ul id="topicslist">
+        {topics.map(topic => (
+          <li key={topic.id}>{topic.title}</li>
+        ))}
+      </ul>
+    );
+  }
   return (
-    <section id="topic">
-      <h3>Topic</h3>
-      <ul id="topic-list"></ul>
-    </section>
+    <div>
+      {content}
+    </div>
+  );
+}
+
+function TopicContent() {
+  const [content, setContent] = useState(null);
+
+useEffect(() => {
+// Fetch the content from the API endpoint
+fetch('api/edai/curriculum/school/subjects/topics/content')
+  .then(response => response.json())
+  .then(data => setContent(data))
+  .catch(error => console.error(error));
+}, []);
+
+if (!content) {
+return <p>Loading...</p>; // Placeholder while the content is being fetched
+}
+
+return (
+<div>
+  {/* Render the fetched content */}
+  <h2>{content.title}</h2>
+  <p>{content.description}</p>
+  {/* Additional content rendering */}
+</div>
+);
+}
+function ContentDisplay() {
+  const isTopicSelected = true;
+
+  let content;
+  if (isTopicSelected) {
+    content = <TopicContent />;
+   } else {
+    content = "Select topic to study";
+   }
+  return (
+    <div>
+      {content}
+    </div>
+  );
+}
+
+function ChatWindow() {
+  return (
+    <div className="chatbot-window">
+      <div className="chatbot-content">
+        <p>Need help? Type or press & hold the mic icon to chat with us!"</p>
+        <div className="chatbot-options">
+          <button className="typing-option">Typing</button>
+          <button className="audio-option">Audio</button>
+      </div>
+      </div>
+    </div>
   );
 }
 
@@ -118,11 +193,23 @@ function App() {
       </header>
       <div className="container">
         <section id="school-topic-window">
-          <SchoolSection />
-          <TopicSection />
+          <section id="school">
+            <h3 id="school-title">Level</h3>
+            <SchoolSection />
+          </section>
+          <section id="topic">
+            <h3>Topic</h3>
+            <TopicDisplay />
+          </section>
         </section>
         <section id="study-window">
           <div id="subjects"></div>
+          <div id="contentdisplay">
+            <ContentDisplay />
+          </div>
+        </section>
+        <section id="chatbot-window">
+          <ChatWindow />
         </section>
       </div>
       <footer></footer>
